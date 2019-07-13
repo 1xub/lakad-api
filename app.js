d4ce9fb4a1fc7148743fd99fe059ffb9d1118abf
jcselorio <joemarie@ubx.ph>
2019-07-13 22:03:28 +0800

    added .env file

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('app-module-path').addPath(require('app-root-path').toString());
+require('dotenv').config();
 const express = require('express');
-// const bodyParser = require('body-parser');
 const app = express();
 
 app.use(
@@ -8,6 +8,6 @@ app.use(
     require('api/routers/EstablismentsRouter')
 );
 
-app.listen(8080, () => {
-  console.log(`Listening to port 8080`);
+app.listen(process.env.APP_PORT || 8080, () => {
+  console.log(`Listening to port ${process.env.APP_PORT}`);
 });
