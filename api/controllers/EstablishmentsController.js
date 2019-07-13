d753f8890ef8a7f8f2a3e7eaaa4a597e9cd7f8d9
jcselorio <joemarie@ubx.ph>
2019-07-13 22:21:17 +0800

    added httpSuccess response

--- api/controllers/EstablishmentsController.js
+++ api/controllers/EstablishmentsController.js
@@ -1,13 +1,22 @@
 require('app-module-path').addPath(require('app-root-path').toString());
 const establishmentsStub = require('seeds/EstablishmentSeeds');
-
+const HttpSuccess = require('api/responses/HttpSuccess');
 /**
  * @param {object} req - Request to the server
  * @param {object} res - Response of the server
  * @param {object} next - Next function to be called
  */
 async function getAllEstablishments(req, res, next) {
-  return res.send(establishmentsStub);
+  try {
+    res.locals.respObj = new HttpSuccess(
+        200,
+        `Successfully retrieve all Supplies`,
+        establishmentsStub
+    );
+    return next();
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 module.exports = {
--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 require('app-module-path').addPath(require('app-root-path').toString());
 require('dotenv').config();
 const express = require('express');
+const HttpSuccess = require('api/responses/HttpSuccess');
 const app = express();
 
-app.use(
-    '/api/lakad',
-    require('api/routers/EstablismentsRouter')
-);
+app.use('/api/lakad', require('api/routers/EstablismentsRouter'));
+
+// Success middleware
+app.use((req, res, next) => {
+  if (res.locals.respObj && res.locals.respObj instanceof HttpSuccess) {
+    return res.status(res.locals.respObj.status).json(res.locals.respObj);
+  }
+  return next();
+});
 
 app.listen(process.env.APP_PORT || 8080, () => {
   console.log(`Listening to port ${process.env.APP_PORT}`);
